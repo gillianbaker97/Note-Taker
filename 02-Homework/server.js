af85418756cd0b4ceb23bcfd9e28bad4d996896d
Gillian Baker <gillianbaker97@gmail.com>
2021-08-04 22:14:11 -0400

    new

--- 02-Homework/server.js
+++ 02-Homework/server.js
@@ -1,19 +1,21 @@
-const express = require('express);
+const express = require('express');
 const PORT = 3001;
-
 const app = express();
 
+// mounts the middleware functions at the specified path
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//app.get routes the HTTP get requests with specific callback functions
 app.get('/notes', (req,res) => {
     console.log('we have the notes');
     return res.json(notes.html);
 });
 
+
 app.get('*', (req,res) => {
     console.log('we have the index');
-    return res.json(index.html);
+    return res.json('index.html');
 });
 
 app.get('/api', (req, res) => {
@@ -22,6 +24,7 @@ app.get('/api', (req, res) => {
     });
 });
 
+// used to bind and listen for the connections between a host and port 
 app.listen(PORT, () => 
     console.log(`Example app listening at http://localhost:${PORT}`)
 );
