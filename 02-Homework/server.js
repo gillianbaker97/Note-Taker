3be7fd1310905ee7249cf1b063695f428fb434fb
Gillian Baker <gillianbaker97@gmail.com>
2021-08-04 20:17:07 -0400

    server.js update

--- 02-Homework/server.js
+++ 02-Homework/server.js
@@ -1,4 +1,27 @@
 const express = require('express);
-const PORT = 3000;
+const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/notes', (req,res) => {
+    console.log('we have the notes');
+    return res.json(notes.html);
+});
+
+app.get('*', (req,res) => {
+    console.log('we have the index');
+    return res.json(index.html);
+});
+
+app.get('/api', (req, res) => {
+    res.json({
+        term: 'api',
+    });
+});
+
+app.listen(PORT, () => 
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
